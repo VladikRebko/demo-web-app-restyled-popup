8eb484c25ab225eaf1a9d27d77b4661eb18e5e47
Vladislav Rebko <v.v.rebko@gmail.com>
2019-01-03 19:08:40 +0300

    added prototype of Table page

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import data from './LeadsData.js';
 
 // Import React Table
 import ReactTable from "react-table";
@@ -6,7 +7,7 @@ import "react-table/react-table.css";
 
 export default class Grid extends Component {
   state = {
-    data: [{}, {}, {}]
+    data: data
   }
 
   renderEditable = (cellInfo) => {
@@ -16,7 +17,7 @@ export default class Grid extends Component {
         style={{ backgroundColor: "#fafafa" }}
         contentEditable
         suppressContentEditableWarning
-        onBlur={e => {
+        onDoubleClick={e => {
           const data = [...this.state.data];
           data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
           this.setState({ data });
@@ -26,7 +27,7 @@ export default class Grid extends Component {
         }}
       />
     );
-  }
+  };
 
   render() {
     const { data } = this.state;
@@ -36,22 +37,97 @@ export default class Grid extends Component {
           data={data}
           columns={[
             {
-              Header: "First Name",
-              accessor: "firstName",
+              Header: "ID",
+              accessor: "ID",
               Cell: this.renderEditable
             },
             {
-              Header: "Last Name",
-              accessor: "lastName",
+              Header: "FirstName",
+              accessor: "FirstName",
               Cell: this.renderEditable
             },
             {
-              Header: "Full Name",
-              id: "full",
+              Header: "LastName",
+              id: "LastName",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "Title",
+              accessor: "Title",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "BirthDay",
+              accessor: "BirthDay",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "Address",
+              accessor: "Address",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "City",
+              accessor: "City",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "Postal",
+              accessor: "Postal",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "Country",
+              accessor: "Country",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "Company",
+              accessor: "Company",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "Email",
+              accessor: "Email",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "Phone",
+              accessor: "Phone",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "Gender",
+              accessor: "Gender",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "Industry",
+              accessor: "Industry",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "WinChance",
+              accessor: "WinChance",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "IsActive",
+              accessor: "IsActive",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "Amount",
+              accessor: "Amount",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "Currency",
+              accessor: "Currency",
               Cell: this.renderEditable
             }
           ]}
-          defaultPageSize={100}
+          defaultPageSize={data.length}
           className="-striped -highlight"
         />
       </div>
