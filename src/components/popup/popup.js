34fb802901c42d02cbd5b6d33e373366921c881b
Vladislav Rebko <v.v.rebko@gmail.com>
2019-01-17 19:25:30 +0300

    added styles for popup

--- src/components/popup/popup.js
+++ src/components/popup/popup.js
@@ -31,7 +31,7 @@ class TablePopup extends Component {
 					<Form
 						onSubmit={this._onSubmit}
 						initialValues={this._getTableElement()}
-						render = {({handleSubmit}) => {
+						render = { ({handleSubmit}) => {
 							return(
 								<div className='popup-content'>
 									<form className='form-for-popup' onSubmit={handleSubmit}>
@@ -39,138 +39,159 @@ class TablePopup extends Component {
 											<div className='fields-container'>
 												
 												<div className= 'input-field-container-for-popup'>
-													<label>Last Name</label>
-													<Field
-														name="lastName"
-														component={TextField}
-														type="text"
-														label="Last Name"
-													/>
+													<label className='label-for-fileld'>Last Name</label>
+													<div className='field-wrapper'>
+														<Field
+															name="lastName"
+															component={TextField}
+															type="text"
+															label="Last Name"
+														/>
+													</div>
 												</div>
 
 												<div className= 'input-field-container-for-popup'>
-													<label>First Name</label>
-													<Field
-														name="firstName"
-														component={TextField}
-														type="text"
-														label="First Name"
-													/>
+													<label className='label-for-fileld'>First Name</label>
+													<div className='field-wrapper'>
+														<Field
+															name="firstName"
+															component={TextField}
+															type="text"
+															label="First Name"
+														/>
+													</div>
 												</div>
 
 												<div className= 'input-field-container-for-popup'>
-													<label>Email</label>
-													<Field
-														name="email"
-														component={TextField}
-														type="text"
-														label="Email"
-													/>
+													<label className='label-for-fileld'>Email</label>
+													<div className='field-wrapper'>
+														<Field
+															name="email"
+															component={TextField}
+															type="text"
+															label="Email"
+														/>
+													</div>
 												</div>
 
 												<div className= 'input-field-container-for-popup'>
-													<label>Phone</label>
-													<Field
-														name="phone"
-														component={TextField}
-														type="text"
-														label="Phone"
-													/>
+													<label className='label-for-fileld'>Phone</label>
+													<div className='field-wrapper'>
+														<Field
+															name="phone"
+															component={TextField}
+															type="text"
+															label="Phone"
+														/>
+													</div>
 												</div>
 
 												<div className= 'input-field-container-for-popup'>
-													<label>Company</label>
-													<Field
-														name="company"
-														component={TextField}
-														type="text"
-														label="Company"
-													/>
+													<label className='label-for-fileld'>Company</label>
+													<div className='field-wrapper'>
+														<Field
+															name="company"
+															component={TextField}
+															type="text"
+															label="Company"
+														/>
+													</div>
 												</div>
 
 												<div className= 'input-field-container-for-popup'>
-													<label>Country</label>
-													<Field className = 'input-field' name="country" component="select">
-														<option />
-														<option value="#ff0000">Belarus</option>
-														<option value="#00ff00">Russia</option>
-														<option value="#0000ff">USA</option>
-													</Field>
+													<label className='label-for-fileld'>Country</label>
+													<div className='field-wrapper'>
+														<Field className = 'input-field' name="country" component="select">
+															<option />
+															<option value="#ff0000">Belarus</option>
+															<option value="#00ff00">Russia</option>
+															<option value="#0000ff">USA</option>
+														</Field>
+													</div>
 												</div>
 
 												<div className= 'input-field-container-for-popup'>
-													<label>Gender</label>
-													<div>
-														<label>
-															<Field
-																name="stooge"
-																component="input"
-																type="radio"
-																value="larry"
-															/>{' '}
-															Female
-														</label>
-														<label>
-															<Field
-																name="stooge"
-																component="input"
-																type="radio"
-																value="moe"
-															/>{' '}
-															Male
-														</label>
-														<label>
-															<Field
-																name="stooge"
-																component="input"
-																type="radio"
-																value="curly"
-															/>{' '}
-															Other
-														</label>
+													<label className='label-for-fileld'>Gender</label>
+													<div className='field-gender-wrapper'>
+															<label className='label-for-gender-field'>
+																	<Field
+																		name="stooge"
+																		component="input"
+																		type="radio"
+																		value="larry"
+																	/>{' '}
+																	Female
+															</label>
+															<label className='label-for-gender-field'>
+																	<Field
+																		name="stooge"
+																		component="input"
+																		type="radio"
+																		value="moe"
+																	/>{' '}
+																	Male
+															</label>
+															<label className='label-for-gender-field'>
+																	<Field
+																		name="stooge"
+																		component="input"
+																		type="radio"
+																		value="curly"
+																	/>{' '}
+																	Other
+															</label>
 													</div>
 												</div>
 											</div>
 											
 											<div className='fields-container'>
 												<div className='input-field-container-for-popup'>
-													<label>Toppings</label>
-													<Field name="toppings" component="select" multiple>
-														<option value="chicken">Accounting</option>
-														<option value="ham">Airlines</option>
-														<option value="mushrooms">Aviation</option>
-														<option value="cheese">Alternative Medicine</option>
-														<option value="tuna">Alternative Dispute Resolution</option>
-													</Field>
+													<label className='label-for-fileld'>Industries</label>
+													<div className='field-wrapper'>
+														<Field name="toppings" component="select" multiple>
+															<option value="chicken">Accounting</option>
+															<option value="ham">Airlines</option>
+															<option value="mushrooms">Aviation</option>
+															<option value="cheese">Alternative Medicine</option>
+															<option value="tuna">Alternative Dispute Resolution</option>
+														</Field>
+													</div>
 												</div>
 												<div className='input-field-container-for-popup'>
-													<label>Win chance</label>
-													<InputRange
-														maxValue={100}
-														minValue={0} />
+													<label className='label-for-fileld'>Win chance</label>
+													<div className='field-wrapper'>
+														<InputRange
+															maxValue={100}
+															minValue={0} />
+													</div>
 												</div>
 												<div className='input-field-container-for-popup'>
-													<label>Status</label>
-													<Field name="employed" component="input" type="checkbox" />
+													<label className='label-for-fileld'>Status</label>
+													<div className='field-wrapper'>
+														<Field name="employed" component="input" type="checkbox" />
+														<label>is active</label>
+													</div>
 												</div>
 											</div>	
 											
 										</div>
-										<div className='buttons'>
-											<Button
-												className = 'submit-button'
-												type="submit"
-												variant="contained"
-												color="primary">
-													Close
-											</Button>
-											<Button
-												className = 'submit-button'
-												type="submit"
-												variant="contained"
-												color="primary">
-												Save
-											</Button>
+										<div className='buttons-wrapper'>
+											<div className='buttons-container'>
+												<Button
+													className = 'submit-button'
+													type="submit"
+													variant="contained"
+													color="primary">
+														Close
+												</Button>
+												<Button
+													className = 'submit-button'
+													type="submit"
+													variant="contained"
+													color="primary">
+													Save
+												</Button>
+											</div>
 										</div>
 									</form>
 								</div>
