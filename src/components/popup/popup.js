a102fb97874407878d8980754280a81c5826e97a
Vladislav Rebko <v.v.rebko@gmail.com>
2019-01-31 17:35:08 +0300

    added simple restyled popup

--- src/components/popup/popup.js
+++ src/components/popup/popup.js
@@ -33,7 +33,7 @@ class TablePopup extends Component {
 	};
 
 	state = {
-		valueOfWinChance: this._getDataForField("WinChance") - 1 + 1,
+		valueOfWinChance: this._getDataForField("WinChance"),
 		selectedCountry: {
 			label:this._getDataForField("Country")
 		},
@@ -47,15 +47,21 @@ class TablePopup extends Component {
 
 	_handleChangeCountry = (selectedCountry) => {
 		this.setState( () =>{ return{selectedCountry} });
-	}
+	};
 
 	_handleChangeIndustry = (selectedIndustry) => {
     this.setState(() =>{ return{selectedIndustry}});
-	}
+	};
 	
 	_handleChangeCurrency = (selectedCurrency) => {
 		this.setState( () =>{ return{selectedCurrency} });
-	}
+	};
+
+	_onWinChanceChange = (value) => {
+		this.setState({
+			valueOfWinChance: value
+ });
+	};
 
 	_onSubmit = (values) =>{
 		const { history } = this.props;
@@ -85,10 +91,29 @@ class TablePopup extends Component {
 
   render(){
 		const { history } = this.props;
-		const { selectedCountry, selectedIndustry, selectedCurrency } = this.state;
+
+		const { 
+			selectedCountry, 
+			selectedIndustry, 
+			selectedCurrency 
+		} = this.state;
 
 		const tableCurrentRow = this._getTableDataRow();
 
+		const { 
+			LastName, 
+			FirstName, 
+			Email, 
+			Phone, 
+			Company, 
+			Country, 
+			Gender, 
+			Industry, 
+			WinChance, 
+			Amount, 
+			Currency
+		} = tableCurrentRow;
+ 
 		if( tableCurrentRow.IsActive === "TRUE" ){
 			tableCurrentRow.IsActive = true;
 		}
@@ -98,209 +123,263 @@ class TablePopup extends Component {
 
 			<div className={'show-popup'}>
 				<div className={'popup-wrapper'}>
-					<Form
-						onSubmit={this._onSubmit}
-						initialValues={tableCurrentRow}
-						render = { ({handleSubmit}) => {
-							return(
-									<form className='form-for-popup' onSubmit={handleSubmit}>
-										<div className='edit-fields'>
-											<div className='fields-container'>
-												
-												<div className= 'input-field-container-for-popup'>
-													<label className='label-for-fileld'>Last Name</label>
-													<div className='field-wrapper'>
-														<Field
-															name="LastName"
-															component={TextField}
-															type="text"
-															label="Last Name"
-														/>
-													</div>
-												</div>
+					<form className='form-for-popup' onSubmit={ () => {console.log('cool')} }>
+						<div className='edit-fields'>
+							<div className='fields-container'>
+								
+							<div className= 'input-field-container-for-popup'>
+									<label className='label-for-fileld'>Last Name</label>
+									<div className='field-wrapper'>
+											{/* <Field
+											name="LastName"
+											component={TextField}
+											type="text"
+											label="Last Name" 
+										/>*/}
 
-												<div className= 'input-field-container-for-popup'>
-													<label className='label-for-fileld'>First Name</label>
-													<div className='field-wrapper'>
-														<Field
-															name="FirstName"
-															component={TextField}
-															type="text"
-															label="First Name"
-														/>
-													</div>
-												</div>
+									<input 
+										type='text'
+										name="LastName"
+										placeholder="LastName"
+										autoComplete="off"
+										defaultValue={LastName}
+									/>
 
-												<div className= 'input-field-container-for-popup'>
-													<label className='label-for-fileld'>Email</label>
-													<div className='field-wrapper'>
-														<Field
-															name="Email"
-															component={TextField}
-															type="text"
-															label="Email"
-														/>
-													</div>
-												</div>
+									</div>
+								</div>
 
-												<div className= 'input-field-container-for-popup'>
-													<label className='label-for-fileld'>Phone</label>
-													<div className='field-wrapper'>
-														<Field
-															name="Phone"
-															component={TextField}
-															type="text"
-															label="Phone"
-														/>
-													</div>
-												</div>
+									<div className= 'input-field-container-for-popup'>
+									<label className='label-for-fileld'>First Name</label>
+									<div className='field-wrapper'>
 
-												<div className= 'input-field-container-for-popup'>
-													<label className='label-for-fileld'>Company</label>
-													<div className='field-wrapper'>
-														<Field
-															name="Company"
-															component={TextField}
-															type="text"
-															label="Company"
-														/>
-													</div>
-												</div>
+									<input 
+										type='text'
+										name="FirstName"
+										placeholder="FirstName"
+										autoComplete="off"
+										defaultValue={FirstName}
+									/>
 
-												<div className= 'input-field-container-for-popup'>
-													<label className='label-for-fileld'>Country</label>
-													<div className='field-wrapper'>
-														<Field 
-															name= "Country"
-															defaultInputValue= {tableCurrentRow.Country}
-															onChange= {this._handleChangeCountry}
-															component={Select} 
-															options={countries}
-															value= {selectedCountry}
-														/>
-													</div>
-												</div>
+									</div>
+								</div>
 
-												<div className= 'input-field-container-for-popup'>
-													<label className='label-for-fileld'>Gender</label>
-													<div className='field-gender-wrapper'>
-															<label className='label-for-gender-field'>
-																	<Field
-																		name="Gender"
-																		component="input"
-																		type="radio"
-																		value="Female"
-																	/>{' '}
-																	Female
-															</label>
-															<label className='label-for-gender-field'>
-																	<Field
-																		name="Gender"
-																		component="input"
-																		type="radio"
-																		value="Male"
-																	/>{' '}
-																	Male
-															</label>
-															<label className='label-for-gender-field'>
-																	<Field
-																		name="Gender"
-																		component="input"
-																		type="radio"
-																		value="Other"
-																	/>{' '}
-																	Other
-															</label>
-													</div>
-												</div>
-											</div>
-											
-											<div className='fields-container'>
-												<div className='input-field-container-for-popup'>
-													<label className='label-for-fileld'>Industries</label>
-													<div className='field-wrapper'>
-														<Field 
-															name="Industry"
-															component={Select} 
-															onChange= {this._handleChangeIndustry}
-															defaultInputValue= {tableCurrentRow.Industry[0]}
-															options={industries}
-															value = {selectedIndustry}
-														/>
-													</div>
-												</div>
-												<div className='input-field-container-for-popup'>
-													<label className='label-for-fileld'>Win chance</label>
-													<div className='field-wrapper'>
-														<InputRange
-															name="WinChance"
-															onChange={ valueOfWinChance => this.setState({ valueOfWinChance }) }
-															maxValue={100}
-															minValue={0} 	
-															value={this.state.valueOfWinChance}
-															/>
-													</div>
-												</div>
-												<div className='input-field-container-for-popup'>
-													<label className='label-for-fileld'>Status</label>
-													<div className='field-wrapper'>
-														<Field 
-															name= "IsActive" 
-															component="input" 
-															type="checkbox"
-														/>
-														<label>is active</label>
-													</div>
-												</div>
-												<div className= 'input-field-container-for-popup'>
-													<label className='label-for-fileld'>Amount</label>
-													<div className='field-wrapper'>
-														<div className= "amount-wrapper">
-															<Field
-																name="Amount"
-																component={TextField}
-																type="text"
-																label="Amount"
-															/>
-															<Field 
-																name= "Currency"
-																defaultInputValue= {tableCurrentRow.Currency}
-																onChange= {this._handleChangeCurrency}
-																component={Select} 
-																options={currency}
-																value= {selectedCurrency}
-															/>
-														</div>
-													</div>
-												</div>
-											</div>	
-											
+								<div className= 'input-field-container-for-popup'>
+									<label className='label-for-fileld'>Email</label>
+									<div className='field-wrapper'>
+
+									<input 
+										type='text'
+										name="Email"
+										placeholder="Email"
+										autoComplete="off"
+										defaultValue={Email}
+									/>
+
+									</div>
+								</div>
+
+								<div className= 'input-field-container-for-popup'>
+									<label className='label-for-fileld'>Phone</label>
+									<div className='field-wrapper'>
+
+									<input 
+										type='text'
+										name="Phone"
+										placeholder="Phone"
+										autoComplete="off"
+										defaultValue={Phone}
+									/>
+									</div>
+								</div>
+
+								<div className= 'input-field-container-for-popup'>
+									<label className='label-for-fileld'>Company</label>
+									<div className='field-wrapper'>
+
+										<input 
+										type='text'
+										name="Company"
+										placeholder="Company"
+										autoComplete="off"
+										defaultValue={Company}
+									/>
+
+									</div>
+								</div>
+
+								<div className= 'input-field-container-for-popup'>
+									<label className='label-for-fileld'>Country</label>
+									<div className='field-wrapper'>
+										{/* <Field 
+											name= "Country"
+											defaultInputValue= {tableCurrentRow.Country}
+											onChange= {this._handleChangeCountry}
+											component={Select} 
+											options={countries}
+											value= {selectedCountry}
+										/> */}
+
+										<select className="select-country">
+											<option>Пункт 1</option>
+											<option>Пункт 2</option>
+										</select>
+									</div>
+								</div>
+
+								<div className= 'input-field-container-for-popup'>
+									<label className='label-for-fileld'>Gender</label>
+									<div className='field-gender-wrapper'>
+											<label className='label-for-gender-field'>
+													{/* <Field
+														name="Gender"
+														component="input"
+														type="radio"
+														value="Female"
+													/> */}
+													<input type="radio" name="Gender" value="Female" />
+													{' '}
+													Female
+
+											</label>
+											<label className='label-for-gender-field'>
+													{/* <Field
+														name="Gender"
+														component="input"
+														type="radio"
+														value="Male"
+													/> */}
+													<input type="radio" name="Gender" value="Male" />
+													{' '}
+													Male
+
+											</label>
+											<label className='label-for-gender-field'>
+													{/* <Field
+														name="Gender"
+														component="input"
+														type="radio"
+														value="Other"
+													/> */}
+													<input type="radio" name="Gender" value="Other" />
+													{' '}
+													Other
+													
+											</label>
+									</div>
+								</div>
+							</div>
+							
+							<div className='fields-container'>
+								<div className='input-field-container-for-popup'>
+									<label className='label-for-fileld'>Industries</label>
+									<div className='field-wrapper'>
+										{/* <Field 
+											name="Industry"
+											component={Select} 
+											onChange= {this._handleChangeIndustry}
+											defaultInputValue= {tableCurrentRow.Industry[0]}
+											options={industries}
+											value = {selectedIndustry}
+										/> */}
+
+										<select className="select-country">
+											<option>Пункт 1</option>
+											<option>Пункт 2</option>
+										</select>
+									</div>
+								</div>
+								<div className='input-field-container-for-popup'>
+									<label className='label-for-fileld'>Win chance</label>
+									<div className='field-wrapper'>
+										{/* <InputRange
+											name="WinChance"
+											onChange={ valueOfWinChance => this.setState({ valueOfWinChance }) }
+											maxValue={100}
+											minValue={0} 	
+											value={this.state.valueOfWinChance}
+											/> */}
+
+									<input 
+										type="range" 
+										min={0}
+										max={100} 
+										step={1} 
+										value={this.state.valueOfWinChance}
+										onChange={ e => this._onWinChanceChange(e.target.value) }
+									/> 
+
+									</div>
+								</div>
+								<div className='input-field-container-for-popup'>
+									<label className='label-for-fileld'>Status</label>
+									<div className='field-wrapper'>
+										{/* <Field 
+											name= "IsActive" 
+											component="input" 
+											type="checkbox"
+										/> */}
+										<input 
+											type="checkbox"
+											name="IsActive"
+											defaultValue={tableCurrentRow.IsActive}
+										/>
+										<label>is active</label>
+									</div>
+								</div>
+								<div className= 'input-field-container-for-popup'>
+									<label className='label-for-fileld'>Amount</label>
+									<div className='field-wrapper'>
+										<div className= "amount-wrapper">
+											{/* <Field
+												name="Amount"
+												component={TextField}
+												type="text"
+												label="Amount"
+											/>
+											<Field 
+												name= "Currency"
+												defaultInputValue= {tableCurrentRow.Currency}
+												onChange= {this._handleChangeCurrency}
+												component={Select} 
+												options={currency}
+												value= {selectedCurrency}  */}
+												<input
+													type="text"
+													name="Amount"
+													placeholder="Amount"
+													autoComplete="off"
+													defaultValue={Amount}
+											/>
+											<select className="select-country" defaultValue={tableCurrentRow.Currency}>
+												<option>Пункт 1</option>
+												<option>Пункт 2</option>
+											</select>
 										</div>
-										<div className='buttons-wrapper'>
-											<div className='buttons-container'>
-												<Button
-													onClick={ () => { history.goBack() } }
-													color="secondary"
-													size="large"
-													variant="outlined">
-														Close
-												</Button>
-												<Button
-													type="submit"
-													size="large"
-													variant="outlined"
-													color="primary">
-													Save
-												</Button>
-											</div>
-										</div>
-									</form>
-							)
-						}}
-					/>
+									</div>
+								</div>
+							</div>	
+						</div>
+						<div className='buttons-wrapper'>
+							<div className='buttons-container'>
+								<Button
+									color="secondary"
+									size="large"
+									variant="outlined">
+										Close
+								</Button>
+								<Button
+									type="submit"
+									size="large"
+									variant="outlined"
+									color="primary">
+									Save
+								</Button>
+							</div>
+						</div>
+					</form>
 				</div>
 			</div>
-		);
+			);
 	}
 }
 
